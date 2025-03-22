2afce3b19239351804cf82dfc7fcb0476caf1c76
ghost <46002572+ghost-itz@users.noreply.github.com>
2025-03-21 21:49:48 -0300

    flatpak.sh

---
+++ flatpak.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+flatpak install flathub com.google.Chrome && flatpak install flathub com.microsoft.Edge && flatpak install flathub com.discordapp.Discord &&
+flatpak install flathub org.telegram.desktop && flatpak install flathub com.spotify.Client && flatpak install flathub app.zen_browser.zen &&
+flatpak install flathub com.ktechpit.orion && flatpak install flathub com.heroicgameslauncher.hgl && flatpak install flathub org.onlyoffice.desktopeditors &&
+flatpak install flathub org.libreoffice.LibreOffice && flatpak install flathub com.github.unrud.VideoDownloader && 
+flatpak install flathub io.github.shiftey.Desktop && flatpak install flathub com.sublimetext.three && flatpak install flathub net.codelogistics.webapps &&
+flatpak install flathub com.todoist.Todoist && flatpak install flathub org.feichtmeier.Musicpod && flatpak install flathub io.github.nate_xyz.Resonance
