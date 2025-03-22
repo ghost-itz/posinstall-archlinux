ee14ef501a34ed31f1c56fecd6ebe1b36de46fa0
ghost <46002572+ghost-itz@users.noreply.github.com>
2025-03-21 23:43:00 -0300

    aplicativos.sh

--- aplicativos.sh
+++ aplicativos.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S gnome-calendar totem eog eog-plugins gnome-text-editor go flatpak audacious alacarte 
+sudo pacman -S gnome-calendar totem evince eog eog-plugins gnome-text-editor gnome-browser-connector gnome-disk-utility baobab gst-plugin-pipewire gnome-system-monitor go git wget flatpak audacious alacarte gst-libav gst-plugins-ugly gst-plugins-good gst-plugins-bad vlc gnome-tweaks  
