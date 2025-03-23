15fe554d7f4d22a219a0206313d0f5fa56b7de27
ghost <46002572+ghost-itz@users.noreply.github.com>
2025-03-23 12:13:55 -0300

    flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-flatpak install flathub com.google.Chrome && flatpak install flathub com.microsoft.Edge && flatpak install flathub com.discordapp.Discord &&
-flatpak install flathub org.telegram.desktop && flatpak install flathub com.spotify.Client && flatpak install flathub app.zen_browser.zen &&
-flatpak install flathub com.ktechpit.orion && flatpak install flathub com.heroicgameslauncher.hgl && flatpak install flathub org.onlyoffice.desktopeditors &&
-flatpak install flathub org.libreoffice.LibreOffice && flatpak install flathub com.github.unrud.VideoDownloader && 
-flatpak install flathub io.github.shiftey.Desktop && flatpak install flathub com.sublimetext.three && flatpak install flathub net.codelogistics.webapps &&
-flatpak install flathub com.todoist.Todoist && flatpak install flathub org.feichtmeier.Musicpod && flatpak install flathub io.github.nate_xyz.Resonance
+flatpak install flathub com.google.Chrome && flatpak install flathub com.microsoft.Edge && flatpak install flathub com.discordapp.Discord --assumeyes &&
+flatpak install flathub org.telegram.desktop --assumeyes && flatpak install flathub app.zen_browser.zen --assumeyes &&
+flatpak install flathub com.heroicgameslauncher.hgl && flatpak install flathub io.github.mmarco94.tambourine --assumeyes &&
+flatpak install flathub org.onlyoffice.desktopeditors --assumeyes && flatpak install flathub com.simplenote.Simplenote --assumeyes  &&
+flatpak install flathub org.libreoffice.LibreOffice --assumeyes && flatpak install flathub com.github.unrud.VideoDownloader --assumeyes && 
+flatpak install flathub io.github.shiftey.Desktop && flatpak install flathub net.codelogistics.webapps --assumeyes &&
+flatpak install flathub com.todoist.Todoist --assumeyes
