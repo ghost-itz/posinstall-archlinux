3076ff82fade3e281a04dd711f2eb1a8d76a6270
ghost <46002572+ghost-itz@users.noreply.github.com>
2025-03-25 01:57:23 -0300

    Add files via upload

---
+++ flatpak.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+flatpak install flathub com.google.Chrome --assumeyes && flatpak install flathub com.discordapp.Discord --assumeyes &&
+flatpak install flathub org.telegram.desktop --assumeyes && flatpak install flathub app.zen_browser.zen --assumeyes &&
+flatpak install flathub io.github.mmarco94.tambourine --assumeyes && flatpak install flathub org.onlyoffice.desktopeditors --assumeyes &&
+flatpak install flathub com.simplenote.Simplenote --assumeyes  && flatpak install flathub org.libreoffice.LibreOffice --assumeyes &&
+flatpak install flathub com.github.unrud.VideoDownloader --assumeyes && flatpak install flathub net.codelogistics.webapps --assumeyes &&
+flatpak install flathub com.todoist.Todoist --assumeyes && flatpak install flathub com.vscodium.codium
---
+++ gaming.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo pacman -S steam lutris wine wine-gecko wine-mono lib32-gnutls gnutls vulkan-icd-loader lib32-vulkan-icd-loader gamemode lib32-gamemode lib32-libpulse
+
