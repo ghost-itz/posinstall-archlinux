2718e5cc8fed4e714fa7662fca66a2b9bd6effe3
ghost <46002572+ghost-itz@users.noreply.github.com>
2025-04-04 21:33:52 -0300

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -3,6 +3,7 @@
 flatpak install flathub com.google.Chrome --assumeyes && flatpak install flathub com.discordapp.Discord --assumeyes &&
 flatpak install flathub org.telegram.desktop --assumeyes && flatpak install flathub app.zen_browser.zen --assumeyes &&
 flatpak install flathub io.github.mmarco94.tambourine --assumeyes && flatpak install flathub org.onlyoffice.desktopeditors --assumeyes &&
-flatpak install flathub com.simplenote.Simplenote --assumeyes  && flatpak install flathub org.libreoffice.LibreOffice --assumeyes &&
-flatpak install flathub com.github.unrud.VideoDownloader --assumeyes && flatpak install flathub com.getmailspring.Mailspring --assumeyes &&
-flatpak install flathub com.todoist.Todoist --assumeyes && flatpak install flathub com.vscodium.codium --assumeyes && flatpak install flathub com.spotify.Client --assumeyes
+flatpak install flathub com.simplenote.Simplenote --assumeyes && flatpak install flathub de.schmidhuberj.tubefeeder -y &&
+flatpak install flathub com.github.unrud.VideoDownloader --assumeyes && flatpak install flathub com.bitwarden.desktop -y &&
+flatpak install flathub com.todoist.Todoist --assumeyes && flatpak install flathub com.vscodium.codium --assumeyes &&
+flatpak install flathub com.warlordsoftwares.tube2go -y
