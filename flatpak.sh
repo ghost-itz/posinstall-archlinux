e408e898711dc805ef0daae0d5d9841a7cf8ffae
ghost-itz <natanaelc.teles05@gmail.com>
2025-04-25 16:44:08 -0300

    update

--- flatpak.sh
+++ flatpak.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-flatpak install flathub com.google.Chrome --assumeyes && flatpak install flathub com.discordapp.Discord --assumeyes &&
-flatpak install flathub org.telegram.desktop --assumeyes && flatpak install flathub app.zen_browser.zen --assumeyes &&
-flatpak install flathub io.github.mmarco94.tambourine --assumeyes && flatpak install flathub org.onlyoffice.desktopeditors --assumeyes &&
-flatpak install flathub com.simplenote.Simplenote --assumeyes && flatpak install flathub com.warlordsoftwares.tube2go -y &&
-flatpak install flathub com.github.unrud.VideoDownloader --assumeyes && flatpak install flathub com.bitwarden.desktop -y &&
-flatpak install flathub com.todoist.Todoist --assumeyes && flatpak install flathub com.vscodium.codium --assumeyes
+flatpak install flathub com.google.Chrome -y && flatpak install flathub com.discordapp.Discord -y &&
+flatpak install flathub org.telegram.desktop -y && flatpak install flathub com.vscodium.codium -y &&
+flatpak install flathub io.github.mmarco94.tambourine -y && flatpak install flathub org.onlyoffice.desktopeditors -y &&
+flatpak install flathub com.simplenote.Simplenote -y && flatpak install flathub com.warlordsoftwares.tube2go -y &&
+flatpak install flathub com.github.unrud.VideoDownloader --y && flatpak install flathub com.bitwarden.desktop -y &&
+flatpak install flathub com.todoist.Todoist -y && flatpak install flathub org.mozilla.firefox -y
 
