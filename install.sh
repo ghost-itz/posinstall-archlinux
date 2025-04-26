5da231bfbf3c2b2a0f90310fe261aabb72a35e44
ghost-itz <natanaelc.teles05@gmail.com>
2025-04-26 09:45:24 -0300

    up

--- install.sh
+++ install.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# Set the locale
-echo "${pt_BR.UTF-8}" >> /etc/locale.gen
-locale-gen
-echo "LANG=${pt_BR.UTF-8}" > /etc/locale.conf
-export LANG=pt_BR.UTF-8
-
 # Set the hostname
 echo "${archlinux}" > /etc/hostname
 
