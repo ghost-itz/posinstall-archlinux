634ee1875b9613ec3256e74e38afda3ad69c7b33
ghost-itz <natanaelc.teles05@gmail.com>
2025-04-26 08:17:35 -0300

    update

--- aplicativos.sh
+++ aplicativos.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-sudo pacman -S gnome-calendar totem evince loupe gnome-text-editor gnome-browser-connector gnome-disk-utility baobab gst-plugin-pipewire gnome-system-monitor go git wget flatpak audacious alacarte gst-libav gst-plugins-ugly gst-plugins-good gst-plugins-bad vlc gnome-tweaks vim qbittorrent gnome-shell-extension-appindicator
+sudo pacman -S gnome-calendar \ #Calendário
+               totem \ #Reprodutor vídeo padrão gnome
+               evince \ #Visualizador de PDF
+               loupe \ #Visualizador de imagem
+               gnome-text-editor \ #Editor de texto
+               gnome-browser-connector \ #Plugin extensão gnome
+               gnome-disk-utility \
+               gparted \
+               baobab \
+               gst-plugin-pipewire \
+               gnome-system-monitor \
+               go \
+               wget \
+               flatpak \
+               audacious \
+               alacarte \
+               gst-libav gst-plugins-ugly gst-plugins-good gst-plugins-bad \
+               vlc \
+               gnome-tweaks \
+               qbittorrent \
+               gnome-shell-extension-appindicator -s
