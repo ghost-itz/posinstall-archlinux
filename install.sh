bd2611dc426132aec35bc0548181df990f97cdbc
ghost-itz <natanaelc.teles05@gmail.com>
2025-04-26 09:04:03 -0300

    up

--- install.sh
+++ install.sh
@@ -3,9 +3,6 @@
 echo LANG=pt_BR.UTF-8 > /etc/locale.conf
 export LANG=pt_BR.UTF-8
 
-mkdir /boot/efi
-mount /dev/vda1 /boot/efi
-
 pacman -S grub-efi-x86 efibootmgr dosfstools mtools wireless_tools \
           noto-fonts noto-fonts-emoji noto-fonts-cjk pipewire-pulse \
           ntfs-3g os-prober ntp gnome-shell gnome-terminal gnome-control-center \
@@ -17,10 +14,3 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchL
 grub-mkconfig -o /boot/grub/grub.cfg
 
 mkinitcpio -p linux-lts
-
-systemctl enable gdm
-systemctl enable NetworkManager
-systemctl enable ntpd
-systemctl enable power-profiles-daemon
-systemctl enable apparmor
-#systemctl enable bluetooth
