36cd1cdea95ecb580cc84bcffe5015bc29af7e26
ghost-itz <natanaelc.teles05@gmail.com>
2025-04-25 22:34:08 -0300

    update

--- nvidia-lts.sh
+++ nvidia-lts.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo pacman -S nvidia-lts nvidia-settings nvidia-utils lib32-nvidia-utils nvtop
+sudo pacman -S intel-ucode nvidia-lts nvidia-settings nvidia-utils lib32-nvidia-utils nvtop
