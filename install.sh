d32906e632a7fbed104874442f8af97e86fcc956
ghost-itz <natanaelc.teles05@gmail.com>
2025-04-26 09:27:39 -0300

    update

--- install.sh
+++ install.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+set -e
+
+pacstrap -i /mnt base base-devel linux-lts linux-firmware nano linux-lts-headers
+genfstab -U -p /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+arch-chroot /mnt /bin/bash <<EOF
+
+# Set the locale
+echo LANG=pt_BR.UTF-8 >> /etc/locale.gen
+locale-gen
 echo LANG=pt_BR.UTF-8 > /etc/locale.conf
 export LANG=pt_BR.UTF-8
+
+# Set the hostname
+echo "${archlinux}" > /etc/hostname
+
+# Create a new user and set the password
+useradd -m -G wheel -s /bin/bash ${ghost}
+echo "${ghost}:${mecanican}" | chpasswd
 
 pacman -S grub-efi-x86 efibootmgr dosfstools mtools wireless_tools \
           noto-fonts noto-fonts-emoji noto-fonts-cjk pipewire-pulse \
@@ -14,3 +32,12 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchL
 grub-mkconfig -o /boot/grub/grub.cfg
 
 mkinitcpio -p linux-lts
+
+systemctl enable gdm
+
+exit
+
+# Unmount filesystems
+umount -R /mnt
+
+reboot
