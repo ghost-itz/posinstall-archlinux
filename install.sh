9bd89bb9ae818f5ee202f3a6f6e107130f4a5e88
ghost-itz <natanaelc.teles05@gmail.com>
2025-04-26 08:49:07 -0300

    update

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo LANG=pt_BR.UTF-8 > /etc/locale.conf
+export LANG=pt_BR.UTF-8
+
+mkdir /boot/efi
+mount /dev/sda1 /boot/efi
+
+pacman -S grub-efi-x86 efibootmgr dosfstools mtools wireless_tools \
+          noto-fonts noto-fonts-emoji noto-fonts-cjk pipewire-pulse \
+          ntfs-3g os-prober ntp gnome-shell gnome-terminal gnome-control-center \
+          gdm gnome-keyring chromium power-profiles-daemon nautilus\
+          networkmanager git apparmor
+
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+mkinitcpio -p linux-lts
+
+systemctl enable gdm
+systemctl enable NetworkManager
+systemctl enable ntpd
+systemctl enable power-profiles-daemon
+systemctl enable apparmor
+#systemctl enable bluetooth
