948066ac3b63ae8f11d70ca9a8496a3706b13dbe
ghost <46002572+ghost-itz@users.noreply.github.com>
2025-04-29 08:46:59 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ pacman -S grub-efi-x86_64 efibootmgr dosfstools mtools wireless_tools \
           noto-fonts noto-fonts-emoji noto-fonts-cjk pipewire-pulse \
           ntfs-3g os-prober ntp gnome-shell gnome-terminal gnome-control-center \
           gdm gnome-keyring chromium power-profiles-daemon nautilus\
-          networkmanager git apparmor
+          networkmanager apparmor
 
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchLinux
 
@@ -18,6 +18,10 @@ grub-mkconfig -o /boot/grub/grub.cfg
 mkinitcpio -p linux-lts
 
 systemctl enable gdm
+systemctl enable NetworkManager
+systemctl enable apparmor
+systemctl enable power-profiles-daemon
+systemctl enable ntpd
 
 exit
 
