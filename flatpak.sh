b8485c4e06cbd7016c00a689739f598344b761ac
ghost <46002572+ghost-itz@users.noreply.github.com>
2025-06-11 19:55:07 -0300

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -5,5 +5,7 @@ flatpak install flathub org.telegram.desktop -y && flatpak install flathub com.v
 flatpak install flathub io.github.mmarco94.tambourine -y && flatpak install flathub org.onlyoffice.desktopeditors -y &&
 flatpak install flathub com.simplenote.Simplenote -y && flatpak install flathub com.warlordsoftwares.tube2go -y &&
 flatpak install flathub com.github.unrud.VideoDownloader -y && flatpak install flathub com.bitwarden.desktop -y &&
-flatpak install flathub com.todoist.Todoist -y && flatpak install flathub org.mozilla.firefox -y
+flatpak install flathub com.todoist.Todoist -y && flatpak install flathub org.mozilla.firefox -y && 
+flatpak install flathub com.rtosta.zapzap -y && flatpak install flathub me.iepure.Ticketbooth -y
+
 
