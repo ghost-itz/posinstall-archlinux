93282ce3d1fb6515428aa8ea855bd4d2f2437e0e
ghost <46002572+ghost-itz@users.noreply.github.com>
2025-12-24 13:28:08 -0300

    Flatpak applications

--- flatpak.sh
+++ flatpak.sh
@@ -3,11 +3,9 @@
 flatpak install flathub com.google.Chrome -y && flatpak install flathub com.discordapp.Discord -y &&
 flatpak install flathub org.telegram.desktop -y && flatpak install flathub com.vscodium.codium -y &&
 flatpak install flathub io.github.mmarco94.tambourine -y && flatpak install flathub org.onlyoffice.desktopeditors -y &&
-flatpak install flathub com.simplenote.Simplenote -y && flatpak install flathub io.github.zaedus.spider -y &&
-flatpak install flathub com.github.unrud.VideoDownloader -y && flatpak install flathub com.bitwarden.desktop -y &&
-flatpak install flathub com.todoist.Todoist -y && flatpak install flathub org.mozilla.firefox -y && 
-flatpak install flathub me.iepure.Ticketbooth -y &&
+&& flatpak install flathub com.bitwarden.desktop -y && flatpak install flathub com.todoist.Todoist -y && 
+flatpak install flathub org.mozilla.firefox -y && flatpak install flathub me.iepure.Ticketbooth -y &&
 flatpak install flathub com.spotify.Client -y && flatpak install flathub org.jdownloader.JDownloader -y &&
-flatpak install flathub com.github.johnfactotum.Foliate -y
+flatpak install flathub it.mijorus.gearlever -y && flatpak install flathub app.zen_browser.zen
 
 
